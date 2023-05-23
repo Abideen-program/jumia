d723cfbb8aaddb9c3a5032e0d05433a7925a8dfd
Abideen Olafimihan <olafimihana@gmail.com>
2023-05-23 22:17:47 +0100

    created the both header for desktop and mobile device

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import Banner from "./Banner";
-import { MdSearch } from "react-icons/md";
+import { MdSearch, MdKeyboardArrowDown } from "react-icons/md";
+import { HiOutlineUser } from "react-icons/hi";
+import { BiHelpCircle } from "react-icons/bi";
+import { AiOutlineShoppingCart } from "react-icons/ai";
+import { GiHamburgerMenu } from "react-icons/gi";
 
 function Header() {
   return (
     <>
       <Banner />
-      <div className="bg-white px-[60px] gap-24 py-5 flex items-center">
+      {/* DESKETOP NAVBAR */}
+      <div className="bg-white hidden px-[50px] gap-16 py-5 lg:flex items-center">
         <div className="h-[50px]">
           <img className="h-full" src="/images/jumia-logo.png" alt="jumia" />
         </div>
@@ -20,15 +25,67 @@ function Header() {
                 placeholder="Search products, brands and categories"
               />
             </div>
-            <button className="bg-[#FF9900] rounded-md text-white px-3 py-2">
+            <button className="bg-[#FF9900] rounded-md text-white px-3 py-2 hover:bg-[#E07E1B]">
               SEARCH
             </button>
           </div>
 
-          <div className="flex items-center border-2 border-stone-950">
-            <MdSearch />
-            <input className="" type="text" />
-            <button>Search</button>
+          <div className="flex items-center gap-3">
+            <div className="flex items-center gap-2 hover:text-[#FF9900] transition-all duration-100">
+              <HiOutlineUser className="text-2xl" />
+              <p>Account</p>
+              <MdKeyboardArrowDown />
+            </div>
+
+            <div className="flex items-center gap-2 hover:text-[#FF9900] transition-all duration-100">
+              <BiHelpCircle className="text-2xl" />
+              <p>Help</p>
+              <MdKeyboardArrowDown />
+            </div>
+
+            <div className="flex items-center gap-2 hover:text-[#FF9900] transition-all duration-100">
+              <AiOutlineShoppingCart className="text-2xl" />
+              <p>Cart</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* MOBILE VIEW */}
+      <div className="bg-white px-[20px] py-5 lg:hidden items-center">
+        <div className="flex items-center justify-between mb-2">
+          <div className="flex gap-2 items-center">
+            <GiHamburgerMenu className="text-2xl" />
+            <div className="h-[20px]">
+              <img
+                className="h-full"
+                src="/images/jumia-logo.png"
+                alt="jumia"
+              />
+            </div>
+          </div>
+
+          <div className="flex items-center gap-3">
+            <div className="flex items-center gap-2 hover:text-[#FF9900] transition-all duration-100">
+              <HiOutlineUser className="text-2xl" />
+            </div>
+
+            <div className="flex items-center gap-2 hover:text-[#FF9900] transition-all duration-100">
+              <AiOutlineShoppingCart className="text-2xl" />
+            </div>
+          </div>
+        </div>
+
+        <div className="flex-1 flex item-center justify-center gap-3">
+          <div className="flex-1 flex item-center justify-center gap-3">
+            <div className="flex items-center border border-[#A3A3A6] rounded-md w-full p-[5px] md:p-[10px]">
+              <MdSearch className="text-2xl mr-[10px] text-[#A3A3A6] font-bold" />
+              <input
+                className="flex-1 border border-none focus:outline-0"
+                type="text"
+                placeholder="Search products, brands and categories"
+              />
+            </div>
           </div>
         </div>
       </div>
