8564e71bb45db5dcdec1d8d3f4886e23c31f6882
Abideen Olafimihan <olafimihana@gmail.com>
2023-05-23 15:55:56 +0100

    setting up initial files

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const App = () => {
+  return <div className="text-red-900 text-lg">App jumia</div>;
+};
+
+export default App;
+
+<svg viewBox="0 0 67 24" class="ic" width="67" height="24"><use xlink:href="https://www.jumia.com.ng/assets_he/images/i-global.04dd5bd2.svg#venture-jumia"></use></svg>
