be0586424ca111fb256ad376ce2e9e4bb1413f5f
Abideen Olafimihan <olafimihana@gmail.com>
2023-06-04 20:03:48 +0100

    added the join us section

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,13 @@
 import FirstSection from "./FirstSection";
 import SecondSection from "./SecondSection";
+import ThirdSection from "./ThirdSection";
 
 const Footer = () => {
   return (
     <div className="mt-10">
       <FirstSection />
       <SecondSection />
+      <ThirdSection />
     </div>
   );
 };
