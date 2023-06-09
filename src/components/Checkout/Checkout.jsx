fe0c230fd4186d85420e9203b72c374c95d0ec10
Abideen Olafimihan <olafimihana@gmail.com>
2023-06-09 22:21:48 +0100

    created the checkout page header

--- src/components/Checkout/Checkout.jsx
+++ src/components/Checkout/Checkout.jsx
@@ -1,7 +1,46 @@
-import React from "react";
+import { Link } from "react-router-dom";
+import { FiPhone, RiArrowGoBackFill } from "react-icons/all";
 
-function Checkout() {
-  return <div>Checkout</div>;
-}
+const Checkout = () => {
+  return (
+    <div className="bg-white px-[50px] py-3 flex items-center justify-between relative right-0 left-0 z-20">
+      <Link to={"/"}>
+        <div className="h-[50px]">
+          <img className="h-full" src="/images/jumia-logo.png" alt="jumia" />
+        </div>
+      </Link>
+
+      <p className="font-semibold">Add New Address</p>
+
+      <div className="flex items-center gap-11 justify-between">
+        <div className="flex items-center gap-2">
+          <FiPhone className="text-[#7A7A7F] text-lg font-bold" />
+          <div>
+            <p className="text-xs text-[#7A7A7F] font-bold">Need Help?</p>
+            <p className="text-xs text-[#294B97] font-bold underline cursor-pointer">
+              Contact us
+            </p>
+          </div>
+        </div>
+
+        <div className="flex items-center gap-2">
+          <RiArrowGoBackFill className="text-[#7A7A7F] font-bold text-lg" />
+          <div>
+            <p className="text-xs text-[#7A7A7F] font-bold">Easy</p>
+            <p className="text-xs text-[#7A7A7F] font-bold">Return</p>
+          </div>
+        </div>
+
+        <div className="flex items-center gap-2">
+          <FiPhone className="text-[#7A7A7F] font-bold text-lg" />
+          <div>
+            <p className="text-xs text-[#7A7A7F] font-bold">Secure</p>
+            <p className="text-xs text-[#7A7A7F] font-bold">Payments</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Checkout;
