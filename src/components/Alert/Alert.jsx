065d150d97a176eaaa429d683f3bf7bfbf20c691
Abideen Olafimihan <olafimihana@gmail.com>
2024-08-25 10:57:54 +0100

    alert adjusted

--- src/components/Alert/Alert.jsx
+++ src/components/Alert/Alert.jsx
@@ -13,14 +13,14 @@ const Alert = () => {
   return (
     <>
       {addNotification && (
-        <div className="bg-[#6dbc28] w-full text-sm font-medium flex gap-2 items-center justify-center py-[10px] text-white fixed top-0 left-0 right-0 z-10">
+        <div className="bg-[#6dbc28] w-full text-sm font-medium flex gap-2 items-center justify-center py-[10px] text-white fixed top-0 left-0 right-0 z-40">
           <MdCheck className="text-base font-medium" />
           <p>Product added successfully</p>
         </div>
       )}
 
       {removeNotification && (
-        <div className="bg-[#6dbc28] w-full text-sm font-medium flex gap-2 items-center justify-center py-[10px] text-white fixed top-0 left-0 right-0 z-10">
+        <div className="bg-[#6dbc28] w-full text-sm font-medium flex gap-2 items-center justify-center py-[10px] text-white fixed top-0 left-0 right-0 z-40">
           <MdCheck className="text-base font-medium" />
           <p>Product removed successfully</p>
         </div>
